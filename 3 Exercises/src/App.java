3c3424b110f4097a70b62d5f10d69fb6a1d0d40f
Henry <36177673+PloKoon2@users.noreply.github.com>
2024-04-15 11:20:29 -0400

    Finished exercise 1

--- 3 Exercises/src/App.java
+++ 3 Exercises/src/App.java
@@ -1,7 +1,16 @@
 import javax.swing.JOptionPane;
 
 public class App {
+    static boolean correct = false;
     public static void main(String[] args) throws Exception{
-        
+        while(!correct){
+            if (JOptionPane.showConfirmDialog(null, "Are whales fish?", "True or False Question",
+            JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(null, "Incorrect. Try again.");
+            } else{
+                JOptionPane.showMessageDialog(null, "Correct. Good job!");
+                correct = true;
+            }
+        }
     }
 }
