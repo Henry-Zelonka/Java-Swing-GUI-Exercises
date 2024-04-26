d42fdaeb06eafe00b3652b1d840f130fbf6dd1db
Henry <36177673+PloKoon2@users.noreply.github.com>
2024-04-26 10:41:48 -0400

    Added Comment

--- 3 Exercises/src/App.java
+++ 3 Exercises/src/App.java
@@ -1,18 +1,28 @@
+//importing JoptionPane to display GUI
 import javax.swing.JOptionPane;
 
 public class App {
+    //start out with the user being incorrect for the true/false question
     static boolean correct = false;
-    GuessNumber guessNumber = new GuessNumber();
+    //main method
     public static void main(String[] args) throws Exception{
+        //loop until the user is correct
         while(!correct){
+            //display a message asking a true or false question
             if (JOptionPane.showConfirmDialog(null, "Are whales fish?", "True or False Question",
+            //get input from the user that is either yes or no
             JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+                //if the user is incorrect, display a message telling them
                 JOptionPane.showMessageDialog(null, "Incorrect. Try again.");
             } else{
+                //if the user is correc, tell them, then set correct to true and exit the loop
                 JOptionPane.showMessageDialog(null, "Correct. Good job!");
                 correct = true;
             }
         }
-        GuessNumber.main(null);
+        //instantiate a GuessNumber object
+        GuessNumber guessNumber = new GuessNumber();
+        //call the main method of GuessNumber
+        guessNumber.guess();
     }
-}
+}        
--- 3 Exercises/src/GuessNumber.java
+++ 3 Exercises/src/GuessNumber.java
@@ -1,18 +1,26 @@
+//importing JoptionPane to display GUI
 import javax.swing.JOptionPane;
 
 public class GuessNumber {
 
+    //generate a random number for the user to guess
     static int number = (int)(Math.random() * 100 + 1);
 
-    public static void main(String args[]){
+    public void guess(){
+        
+        //guess variable starts outside the range of numbers that can be generated so it is guaranteed not ot match the number
         int guess = 200;
 
+        //loop while the user's guess is not equal to the correct number
         while(guess != number){
 
+            //ask the user to guess a number within the range of 1 to 100
             guess = Integer.parseInt(JOptionPane.showInputDialog("Guess a number from 1 to 100:"));
 
+            //if the user's guess it too high or low, tell them
             if(guess > number)JOptionPane.showMessageDialog(null, "Too high!");
             else if(guess < number)JOptionPane.showMessageDialog(null, "Too low!");
+            //if the user guesses the correct number, tell them
             else JOptionPane.showMessageDialog(null, "Correct, the number was " + number + "!");
 
         }
